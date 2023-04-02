842b41286e1d55d7fd65a60288bffb25a37d0f5e
doinki <doinki94@gmail.com>
2023-04-02 11:33:47 +0000

    feat(lotto): Add DrawerResult component

---
+++ apps/lotto/src/components/DrawerResult/DrawerResult.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react';
+import { memo } from 'react';
+
+import type { DrawerResultProps } from './drawerResultTypes';
+
+const DrawerResult: FC<DrawerResultProps> = (props) => {
+  const { round, winningNumbers } = props;
+
+  return (
+    <article className="space-y-6">
+      <h1 className="text-center text-4xl">{round || '-'}회 당첨번호</h1>
+      <strong className="block space-x-6 text-center">
+        {winningNumbers.map((number, index) => (
+          <span
+            key={number}
+            className={
+              index === winningNumbers.length - 1 ? 'text-blue-500' : undefined
+            }
+          >
+            {number || '-'}
+          </span>
+        ))}
+      </strong>
+    </article>
+  );
+};
+
+export default memo(DrawerResult);
