341cf6523e849407455d7cfb9fc6b2c74e59f863
doinki <doinki94@gmail.com>
2023-04-03 23:19:11 +0900

    feat(lotto): Enable scrollRestoration in next

--- apps/lotto/src/pages/_app.tsx
+++ apps/lotto/src/pages/_app.tsx
@@ -2,8 +2,19 @@ import '@/styles/tailwind.css';
 
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import { useEffect } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isSafari =
+      userAgent.includes('safari') && !userAgent.includes('chrom');
+
+    if (isSafari) {
+      window.history.scrollRestoration = 'auto';
+    }
+  }, []);
+
   return (
     <>
       <DefaultSeo defaultTitle="Lotto" titleTemplate="%s - Lotto" />
