4f790d1230e14d36f3b7980b2a347ba9ef63757c
doinki <doinki94@gmail.com>
2023-04-07 18:57:40 +0900

    🚀

--- packages/ui/src/Skeleton/Skeleton.stories.tsx
+++ packages/ui/src/Skeleton/Skeleton.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Skeleton } from '../src';
+import Skeleton from './Skeleton';
 
 const meta: Meta<typeof Skeleton> = {
   component: Skeleton,
