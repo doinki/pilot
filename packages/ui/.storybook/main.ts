ca1e9796b6a9273b0a9fd8de5c7e5090651952d4
doinki <doinki94@gmail.com>
2023-04-07 22:20:52 +0900

    🚀

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -12,6 +12,10 @@ const config: StorybookConfig = {
   ],
   docs: { autodocs: 'tag' },
   framework: { name: '@storybook/react-webpack5', options: {} },
+  refs: (_, { configType }) =>
+    configType === 'DEVELOPMENT'
+      ? { icons: { title: 'Icons', url: 'http://localhost:6007' } }
+      : { icons: { title: 'Icons', url: process.env.URL! } },
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(j|t)s?(x)'],
 };
 
