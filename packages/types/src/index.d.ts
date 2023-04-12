94d4167fa7fca28c34b604f9b554cc779785a00d
doinki <doinki94@gmail.com>
2023-04-12 22:33:04 +0900

    🚀

--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -1,2 +1,3 @@
 export * from './next';
 export * from './react';
+export * from './utility';
---
+++ packages/types/src/utility/array.d.ts
@@ -0,0 +1,13 @@
+export type Join<
+  Strings extends ReadonlyArray<string | number>,
+  Delimiter extends string
+> = Strings extends []
+  ? ''
+  : Strings extends readonly [string | number]
+  ? `${Strings[0]}`
+  : Strings extends readonly [
+      string | number,
+      ...infer Rest extends ReadonlyArray<string | number>
+    ]
+  ? `${Strings[0]}${Delimiter}${Join<Rest, Delimiter>}`
+  : string;
