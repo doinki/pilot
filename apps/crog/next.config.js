94f4513560862f8444f554084da6608e5f6ec296
doinki <doinki94@gmail.com>
2023-04-21 21:52:43 +0900

    🚀

--- apps/crog/next.config.js
+++ apps/crog/next.config.js
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
+  compress: process.env.NODE_ENV === 'development',
   eslint: { ignoreDuringBuilds: true },
   experimental: { scrollRestoration: true },
   output: 'standalone',
