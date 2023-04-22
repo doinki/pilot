40639ba22f59d251a2624761046817755be66419
doinki <doinki94@gmail.com>
2023-04-22 12:22:59 +0900

    🚀

--- packages/icons/.storybook/main.ts
+++ packages/icons/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   addons: [
+    '@storybook/addon-a11y',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-links',
