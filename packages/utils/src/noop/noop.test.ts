40f40054c26e840e8cbb24d984574184aaa561f9
doinki <doinki94@gmail.com>
2023-04-24 00:25:17 +0900

    🚀

---
+++ packages/utils/src/noop/noop.test.ts
@@ -0,0 +1,10 @@
+import noop from './noop';
+
+describe('noop', () => {
+  test.each([0, 0n, '', false, Symbol(''), null, undefined, {}])(
+    'should return the value passed to it',
+    (value) => {
+      expect(noop(value)).toBe(value);
+    }
+  );
+});
