87d8cc0456403bda3f8b685589f0df8b299e0d38
doinki <doinki94@gmail.com>
2023-04-24 23:52:30 +0900

    🚀

--- apps/crog/src/pages/_document.tsx
+++ apps/crog/src/pages/_document.tsx
@@ -1,6 +1,9 @@
+import { randomUUID } from 'crypto';
 import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => {
+  const nonce = randomUUID();
+
   return (
     <Html lang="ko">
       <Head>
@@ -22,11 +25,12 @@ const Document = () => {
             __html:
               '(()=>{let e="dark",t="theme",c=t=>{t?document.documentElement.classList.add(e):document.documentElement.classList.remove(e)};window.__setPreferredTheme=a=>{c(a===e);try{localStorage.setItem(t,a)}catch(e){}};let a=matchMedia("(prefers-color-scheme: dark)"),m=a=>{let m;try{m=localStorage.getItem(t)}catch(e){}c(m===e||null==m&&a)};m(a.matches),a.addEventListener("change",e=>{m(e.matches)})})();',
           }}
+          nonce={nonce}
         />
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
       </body>
     </Html>
   );
