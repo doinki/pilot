4d1640ecba1e8ad20a3c44a976db0d82cb51348f
doinki <doinki94@gmail.com>
2023-05-09 00:42:42 +0900

    🚀

--- apps/challenge/tailwind.config.js
+++ apps/challenge/tailwind.config.js
@@ -1,19 +1,10 @@
+const tailwindConfig = require('@pilot/tailwindcss/tailwind.config');
+
 /**
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
-    },
-  },
+  ...tailwindConfig,
+  content: ['../../packages/ui/src/**/*.@(j|t)s?(x)', 'src/**/*.@(j|t)s?(x)'],
+  darkMode: 'class',
 };
