107cddffb214529087a54c5ce9e0b7c17c6292e2
doinki <doinki94@gmail.com>
2023-05-08 21:49:51 +0900

    🚀

--- apps/challenge/next.config.js
+++ apps/challenge/next.config.js
@@ -1,4 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = { experimental: { appDir: true } };
+module.exports = {
+  compress: process.env.NODE_ENV === 'development',
+  eslint: { ignoreDuringBuilds: true },
+  experimental: { scrollRestoration: true },
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  typescript: { tsconfigPath: 'tsconfig.build.json' },
+};
