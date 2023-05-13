6733748b3b78435010a85eddd0394fe76a0aa18c
doinki <doinki94@gmail.com>
2023-05-13 19:47:59 +0900

    🚀

--- apps/challenge/src/app/providers.tsx
+++ apps/challenge/src/app/providers.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { useExperimentalScrollRestoration } from '@pilot/hooks';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { FC, ReactNode } from 'react';
+import { useState } from 'react';
 
 const Providers: FC<{ children?: ReactNode }> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   useExperimentalScrollRestoration();
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 };
 
 export default Providers;
