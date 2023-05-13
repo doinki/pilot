c5df7f9fe80f517cdd6f38a27312e484288668bf
doinki <doinki94@gmail.com>
2023-05-14 00:24:01 +0900

    🚀

--- apps/challenge/next.config.js
+++ apps/challenge/next.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   compress: process.env.NODE_ENV === 'development',
   eslint: { ignoreDuringBuilds: true },
-  experimental: { scrollRestoration: true },
+  experimental: { scrollRestoration: true, typedRoutes: true },
   output: 'standalone',
   poweredByHeader: false,
   typescript: { tsconfigPath: 'tsconfig.build.json' },
