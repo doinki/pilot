6d95217a574c71fcb67a1a4858b89c8194fe3c42
doinki <doinki94@gmail.com>
2023-05-22 01:06:16 +0900

    🚀

--- apps/challenge/src/fonts/index.ts
+++ apps/challenge/src/fonts/index.ts
@@ -2,7 +2,7 @@ import { Noto_Sans_KR } from 'next/font/google';
 
 export const notoSansKR = Noto_Sans_KR({
   adjustFontFallback: false,
-  subsets: [],
+  subsets: ['latin'],
   variable: '--font-noto-sans-kr',
   weight: ['400', '500', '700'],
 });
