abad9fd576f240dcfaf0c47e33dce0aacf1748e5
doinki <doinki94@gmail.com>
2023-05-26 20:30:53 +0900

    🚀

--- apps/challenge/next.config.js
+++ apps/challenge/next.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   eslint: { ignoreDuringBuilds: true },
-  experimental: { scrollRestoration: true, typedRoutes: true },
+  experimental: { typedRoutes: true },
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 30 * 24 * 60 * 60,
--- apps/challenge/src/app/Providers.tsx
+++ apps/challenge/src/app/Providers.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useExperimentalScrollRestoration } from '@pilot/hooks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { FC, ReactNode } from 'react';
 import { useState } from 'react';
 
 const Providers: FC<{ children?: ReactNode }> = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient());
-
-  useExperimentalScrollRestoration();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
