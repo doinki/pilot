cc191c7170f2b299c75eac215c3bb7c372e597e2
doinki <doinki94@gmail.com>
2023-06-04 00:28:42 +0900

    🚀

--- apps/challenge/scripts/download.js
+++ apps/challenge/scripts/download.js
@@ -1,22 +1,16 @@
 const { DomUtils, parseDocument } = require('htmlparser2');
-const { concatMap, delay, map, mergeMap, range, retry } = require('rxjs');
+const { delay, map, mergeMap, range, retry } = require('rxjs');
 const { fromFetch } = require('rxjs/fetch');
 
 range(1, 1010)
   .pipe(
-    map((id) => id.toString().padStart(4, '0')),
+    map(String),
+    map((id) => id.padStart(4, '0')),
     mergeMap(
       (id) =>
-        fromFetch(`https://zukan.pokemon.co.jp/detail/${id}`).pipe(
-          delay(500),
-          concatMap((response) => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok.');
-            }
-            return response.text();
-          }),
-          retry(3)
-        ),
+        fromFetch(`https://zukan.pokemon.co.jp/detail/${id}`, {
+          selector: (response) => response.text(),
+        }).pipe(delay(500), retry(3)),
       2
     ),
     map((html) =>
