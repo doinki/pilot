0f75c484819459452afa553dff4e54622d951546
doinki <doinki94@gmail.com>
2023-06-07 14:40:48 +0900

    🚀

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -2,6 +2,7 @@
  * @type {import('jest').Config}
  */
 module.exports = {
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts'],
   testEnvironment: 'jsdom',
   transform: { '\\.(j|t)s$': '@swc/jest' },
 };
