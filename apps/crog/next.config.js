e6927964d32210d8026f82dfe3b05e15ee85bec3
doinki <doinki94@gmail.com>
2023-06-11 19:50:43 +0900

    🚀

--- apps/crog/next.config.js
+++ apps/crog/next.config.js
@@ -4,7 +4,6 @@
 module.exports = {
   compress: process.env.NODE_ENV === 'development',
   eslint: { ignoreDuringBuilds: true },
-  experimental: { scrollRestoration: true, typedRoutes: true },
   output: 'standalone',
   poweredByHeader: false,
   typescript: { tsconfigPath: 'tsconfig.build.json' },
