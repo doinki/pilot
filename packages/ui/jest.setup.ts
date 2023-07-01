36a5b5f0e96c9dd5c82ab9720d835ca0e540179c
doinki <doinki94@gmail.com>
2023-07-01 23:33:02 +0900

    chore(ui): Turn off jest snapshot

--- packages/ui/jest.setup.ts
+++ packages/ui/jest.setup.ts
@@ -1,19 +1,1 @@
 import '@testing-library/jest-dom';
-
-import initStoryshots, {
-  multiSnapshotWithOptions,
-} from '@storybook/addon-storyshots';
-import type { RenderResult } from '@testing-library/react';
-import { render } from '@testing-library/react';
-
-const reactTestingLibrarySerializer: jest.SnapshotSerializerPlugin = {
-  print: (val, serialize) =>
-    serialize((val as RenderResult).container.firstChild),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  test: (val) => val && Object.prototype.hasOwnProperty.call(val, 'container'),
-};
-
-initStoryshots({
-  snapshotSerializers: [reactTestingLibrarySerializer],
-  test: multiSnapshotWithOptions({ renderer: render }),
-});
