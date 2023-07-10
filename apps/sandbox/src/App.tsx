df6751cdf5d3454a9211b956a3f08313e3ae9b07
doinki <doinki94@gmail.com>
2023-07-11 00:11:37 +0900

    🚀

--- apps/sandbox/src/App.tsx
+++ apps/sandbox/src/App.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   { lazy: () => import('./pages/Home'), path: '/' },
-  { lazy: () => import('./pages/About'), path: 'about' },
 ]);
 
 const App = () => {
--- apps/sandbox/src/pages/Home.tsx
+++ apps/sandbox/src/pages/Home.tsx
@@ -1,17 +1,5 @@
 import type { FC } from 'react';
-import { Link } from 'react-router-dom';
 
 export const Component: FC = () => {
-  return (
-    <div>
-      <h1 className="text-center text-4xl">Home</h1>
-      <nav>
-        <ul style={{ listStyle: 'inside' }}>
-          <li>
-            <Link to="about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <h1 className="my-6 text-center text-4xl">Home</h1>;
 };
